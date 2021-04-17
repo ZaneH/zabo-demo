5309ec4387c0c50abd4818708f2fde64750a4d78
Zane Helton <zane99@me.com>
2021-04-17 14:35:11 -0400

    Ran Prettier

--- zabo-client-demo/src/components/Taskbar.jsx
+++ zabo-client-demo/src/components/Taskbar.jsx
@@ -3,22 +3,22 @@ import { AppBar, Toolbar, Button } from 'react95';
 import zaboImg from '../img/zabo.png';
 
 const Taskbar = () => {
-	return (
-		<AppBar style={{ top: 'auto', bottom: 0 }}>
-			<Toolbar style={{ justifyContent: 'space-between' }}>
-				<div style={{ position: 'relative', display: 'inline-block' }}>
-					<Button>
-						<img
-							src={zaboImg}
-							alt="Zabo logo"
-							style={{ height: '20px', marginRight: 6 }}
-						/>
-						Start
-					</Button>
-				</div>
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <AppBar style={{ top: 'auto', bottom: 0 }}>
+      <Toolbar style={{ justifyContent: 'space-between' }}>
+        <div style={{ position: 'relative', display: 'inline-block' }}>
+          <Button>
+            <img
+              src={zaboImg}
+              alt="Zabo logo"
+              style={{ height: '20px', marginRight: 6 }}
+            />
+            Start
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Taskbar;
--- zabo-client-demo/src/components/ZaboLoader.jsx
+++ zabo-client-demo/src/components/ZaboLoader.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import ZaboProvider, { ZaboContext } from './ZaboProvider';
 
 const ZaboLoader = ({ children }) => {
-	return (
-		<ZaboProvider>
-			<ZaboContext.Consumer>
-				{(zabo) => {
-					if (zabo.loading) {
-						return <p>Loading...</p>;
-					} else if (zabo.error) {
-						return <p>{zabo.error.toString()}</p>;
-					} else {
-						return children;
-					}
-				}}
-			</ZaboContext.Consumer>
-		</ZaboProvider>
-	);
+  return (
+    <ZaboProvider>
+      <ZaboContext.Consumer>
+        {(zabo) => {
+          if (zabo.loading) {
+            return <p>Loading...</p>;
+          } else if (zabo.error) {
+            return <p>{zabo.error.toString()}</p>;
+          } else {
+            return children;
+          }
+        }}
+      </ZaboContext.Consumer>
+    </ZaboProvider>
+  );
 };
 
 export default ZaboLoader;
