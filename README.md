055533f14cada0ff5f6903b9ba9741bac99b2ee8
Zane Helton <zane99@me.com>
2021-04-17 14:43:40 -0400

    Updated README and added a link to Zabo

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ A simple demo to read the balance from a connected wallet or a specified Ethereu
 
 ## Preview
 ![GIF](https://i.imgur.com/72TCjiP.gif)
+
+## Run Demo
+1. Open 2 Terminal windows
+2. `cd` into `zabo-backend-demo`
+    - Create a .env file within
+      - Add `ZABO_SECRET_KEY=your_secret_key`
+      - Add `ZABO_API_KEY=your_api_key`
+    - Run `npm i`
+    - Run `npm run start`
+3. `cd` into `zabo-client-demo`
+    - Create a .env file within
+      - Add `REACT_APP_ZABO_CLIENT_ID=your_client_id`
+    - Run `npm i`
+    - Run `npm run start`
--- zabo-client-demo/src/components/Taskbar.jsx
+++ zabo-client-demo/src/components/Taskbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Button } from 'react95';
+import { AppBar, Toolbar, Button, Anchor } from 'react95';
 import zaboImg from '../img/zabo.png';
 
 const Taskbar = () => {
@@ -7,14 +7,16 @@ const Taskbar = () => {
     <AppBar style={{ top: 'auto', bottom: 0 }}>
       <Toolbar style={{ justifyContent: 'space-between' }}>
         <div style={{ position: 'relative', display: 'inline-block' }}>
-          <Button>
-            <img
-              src={zaboImg}
-              alt="Zabo logo"
-              style={{ height: '20px', marginRight: 6 }}
-            />
-            Start
-          </Button>
+          <Anchor href="https://zabo.com" target="_blank">
+            <Button>
+              <img
+                src={zaboImg}
+                alt="Zabo logo"
+                style={{ height: '20px', marginRight: 6 }}
+              />
+              Start
+            </Button>
+          </Anchor>
         </div>
       </Toolbar>
     </AppBar>
